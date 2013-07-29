4b61506f06dd8dd25ac08ce720d2c6b9bca12af0
BuchaCyrill <BuchaCyrill@gmail.com>
2013-07-29 11:20:16 +0300

    --Coomit

--- Kiryl_Bucha_generate/generate/for_i_tmp_opers.sql
+++ Kiryl_Bucha_generate/generate/for_i_tmp_opers.sql
@@ -5,44 +5,47 @@ BEGIN
       dbms_random.initialize ( c * 2 );
 
       INSERT INTO tmp_opers
-         SELECT 
-               TRUNC ( SYSTIMESTAMP )
-                - dbms_random.VALUE ( 1
+       SELECT   TRUNC ( SYSTIMESTAMP )
+          - dbms_random.VALUE ( 1
+                              , 5000 )
+             AS event_dt
+        , tr.rn AS transaction_id
+        , ROUND ( dbms_random.VALUE ( 1
+                                    , ( SELECT COUNT ( * )
+                                          FROM tmp_products ) ) )
+             AS cntr_str
+        , ROUND ( dbms_random.VALUE ( 1
+                                    , ( SELECT COUNT ( * )
+                                          FROM tmp_products ) ) )
+             AS cntr_prd
+        , ROUND ( dbms_random.VALUE ( 1
+                                    , ( SELECT COUNT ( * )
+                                          FROM tmp_customers ) ) )
+             AS emp_num
+        , ROUND ( dbms_random.VALUE ( 1
+                                    , ( SELECT COUNT ( * )
+                                          FROM tmp_customers ) ) )
+             AS cust_num
+        , ROUND ( dbms_random.VALUE ( 1
+                                    , 4 ) )
+             AS pay_metod
+        , ROUND ( dbms_random.VALUE ( 1
+                                    , 10 ) )
+             AS quantity
+        , ROUND ( dbms_random.VALUE ( 1
+                                    , 50000000 ) )
+             AS check_num
+        , ROUND ( dbms_random.VALUE ( 1
                                     , 5000 )
-                   AS event_dt
-              , tr.rn AS transaction_id
-              , ROUND ( dbms_random.VALUE ( 1
-                                          , ( SELECT COUNT ( * ) FROM tmp_stores ) ) )
-                   AS cntr_str
-              , ROUND ( dbms_random.VALUE ( 1
-                                          , ( SELECT COUNT ( * ) FROM tmp_products ) ) )
-                   AS cntr_prd
-              , ROUND ( dbms_random.VALUE ( 1
-                                          , ( SELECT COUNT ( * ) FROM tmp_emp ) ) )
-                   AS emp_num
-              , ROUND ( dbms_random.VALUE ( 1
-                                          , ( SELECT COUNT ( * ) FROM tmp_cust ) ) )
-                   AS cust_num
-              , ROUND ( dbms_random.VALUE ( 1
-                                          , 4 ) )
-                   AS pay_metod
-              , ROUND ( dbms_random.VALUE ( 1
-                                          , 10 ) )
-                   AS quantity
-              , ROUND ( dbms_random.VALUE ( 1
-                                          , 50000000 ) )
-                   AS check_num
-              , ROUND ( dbms_random.VALUE ( 1
-                                          , 5000 )
-                      , 2 )
-                   AS price
-              , TRUNC ( dbms_random.VALUE ( 1
-                                          , 20 ) )
-                / 100
-                   AS disc
-           FROM (    SELECT ROWNUM AS rn
-                       FROM DUAL
-                 CONNECT BY ROWNUM <= 10000) tr;
+                , 2 )
+             AS price
+        ,   TRUNC ( dbms_random.VALUE ( 1
+                                      , 20 ) )
+          / 100
+             AS disc
+     FROM (SELECT     ROWNUM AS rn
+                 FROM DUAL
+           CONNECT BY ROWNUM <= 10000) tr;
 
       COMMIT;
    END LOOP;
