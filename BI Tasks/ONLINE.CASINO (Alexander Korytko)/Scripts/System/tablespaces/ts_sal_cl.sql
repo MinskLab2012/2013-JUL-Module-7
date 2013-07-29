7d237f01072a69536d2673e9a94dddf8581f97f9
BuchaCyrill <BuchaCyrill@gmail.com>
2013-07-29 10:10:47 +0300

    Delete

--- BI Tasks/ONLINE.CASINO (Alexander Korytko)/Scripts/System/tablespaces/ts_sal_cl.sql
+++
@@ -1,8 +0,0 @@
-drop tablespace TS_SAL_CL including contents cascade constraints;
-
-create tablespace TS_SAL_CL
-datafile 'db_sal_dw_cl.dat'
-size 100M
- autoextend on next 50M
-nologging
-segment space management auto;
