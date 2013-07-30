c1a39009517d6e4d0395a295cf5d9e66144e0831
VasiliKorzun <vykorzun@gmail.com>
2013-07-30 12:43:30 +0300

    Added Task 2 scripts

--- Task 02/Vasili Korzun/Daily report script.sql
+++ Task 02/Vasili Korzun/Daily report script.sql
@@ -1,8 +1,8 @@
- SELECT 
-        DECODE ( gid, 7, 'Total: ', day_id ) as event_date
-       , DECODE ( gid, 3, 'Total for ' || prod_name, prod_name ) as product
-       , DECODE ( gid, 1, 'Total for ' || delivery_system_code, delivery_system_code ) as delivery_system
-       , payment_system_desc as payment_system
+/* Formatted on 7/30/2013 12:42:45 PM (QP5 v5.139.911.3011) */
+  SELECT DECODE ( gid, 7, 'Total: ', day_id ) AS event_date
+       , DECODE ( gid, 3, 'Total for ' || prod_name, prod_name ) AS product
+       , DECODE ( gid, 1, 'Total for ' || delivery_system_code, delivery_system_code ) AS delivery_system
+       , payment_system_desc AS payment_system
        , amount_sold
        , cnt
     FROM (  SELECT GROUPING_ID ( prod_name
@@ -23,5 +23,6 @@
                AND country = :p_country
           GROUP BY ROLLUP ( ( TRUNC ( transaction_dt
                                     , 'dd' ), prod_name ), delivery_system_code, payment_system_desc ))
-                                    ORDER BY day_id,prod_name, gid
-;
+ORDER BY day_id
+       , prod_name
+       , gid;
--- Task 02/Vasili Korzun/Monthly report script.sql
+++ Task 02/Vasili Korzun/Monthly report script.sql
@@ -1,33 +1,28 @@
-/* Formatted on 7/30/2013 11:58:08 AM (QP5 v5.139.911.3011) */
-
+/* Formatted on 7/30/2013 12:42:39 PM (QP5 v5.139.911.3011) */
  -- In Oracle BI Publisher module we've studied how to NOT to write such queries.
  -- Totals and subtotals should be added in Template, not in Data model or DWH.
 
-  SELECT gid
-       , DECODE ( gid, 3, 'Grand total', 1, 'Total by '||month_id, month_id ) AS event_date
-       , DECODE ( gid, 2, 'Total for ' || payment_system_desc, payment_system_desc ) AS payment_system_desc
-       , amount_sold
-       , cnt
-    FROM (        SELECT GROUPING_ID (  TRUNC ( transaction_dt
-                         , 'mm' ), payment_system_desc )
-                      AS gid
-                 , TRUNC ( transaction_dt
-                         , 'mm' )
-                      AS month_id
-                 , payment_system_desc
-                 , SUM ( cost ) AS amount_sold
-                 , COUNT ( transaction_id ) AS cnt
-              FROM tmp_orders
-             WHERE EXTRACT ( YEAR FROM transaction_dt ) = :p_year
-               AND country = :P_COUNTRY
-          GROUP BY  CUBE (TRUNC ( transaction_dt
-                                  , 'mm' )
-                 ,  payment_system_desc )
-order by month_id, gid, payment_system_desc);
-       
-       
-       
-       
-       
-       
-       
+SELECT --gid,
+      DECODE ( gid,  3, 'Grand total',  1, 'Total by ' || month_id,  month_id ) AS event_date
+     , DECODE ( gid, 2, 'Total for ' || payment_system_desc, payment_system_desc ) AS payment_system_desc
+     , amount_sold
+     , cnt
+  FROM (  SELECT GROUPING_ID ( TRUNC ( transaction_dt
+                                     , 'mm' )
+                             , payment_system_desc )
+                    AS gid
+               , TRUNC ( transaction_dt
+                       , 'mm' )
+                    AS month_id
+               , payment_system_desc
+               , SUM ( cost ) AS amount_sold
+               , COUNT ( transaction_id ) AS cnt
+            FROM tmp_orders
+           WHERE EXTRACT ( YEAR FROM transaction_dt ) = :p_year
+             AND country = :p_country
+             AND payment_system_desc IN ('PayPal', 'WebMoney')
+        GROUP BY CUBE ( TRUNC ( transaction_dt
+                              , 'mm' ), payment_system_desc )
+        ORDER BY month_id
+               , gid
+               , payment_system_desc);
