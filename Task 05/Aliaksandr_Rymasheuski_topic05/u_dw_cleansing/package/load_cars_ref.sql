f7ac13cd9b514732454c1dfeb03dd6572e6f5755
AliaksandrRymasheuski <arimashevsky@gmail.com>
2013-08-07 13:13:27 +0300

    add package of loading the transaction table

--- Task 05/Aliaksandr_Rymasheuski_topic05/u_dw_cleansing/package/load_cars_ref.sql
+++
@@ -1,6 +0,0 @@
-BEGIN
-   pkg_load_ext_ref_cars.load_brands;
-   pkg_load_ext_ref_cars.load_models;
-   pkg_load_ext_ref_cars.load_cars;
-END;
-/
--- Task 05/Aliaksandr_Rymasheuski_topic05/u_dw_cleansing/package/pkg_load_ext_ref_cars-def.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE PACKAGE pkg_load_ext_ref_cars
--- Package Reload Data From External Sources to DataBase
-AS
-   PROCEDURE load_brands;
-  PROCEDURE load_models;
-  PROCEDURE load_cars;
-END;
-/
